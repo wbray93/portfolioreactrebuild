fd8cec905e92b94bd5408297fb9da3dfadf35487
wbray93 <wbray053@gmail.com>
2018-09-11 15:35:57 -0700

    Fixed social media links

--- src/components/social media/index.jsx
+++ src/components/social media/index.jsx
@@ -2,10 +2,10 @@ import * as React from "react";
 
 const Social = () => (
   <div className="social-media">
-    <i className="fab fa-facebook icon-size" />
-    <i className="fab fa-linkedin icon-size" />
-    <i className="fab fa-github-square icon-size" />
-    <i className="fab fa-twitter icon-size" />
+    <a href="https://www.facebook.com/Whitehawk-Industries-1817186608317870/"><i className="fab fa-facebook icon-size" /></a>
+    <a href="https://www.linkedin.com/in/william-bray-2b47a2157/"><i className="fab fa-linkedin icon-size" /></a>
+    <a href="https://github.com/wbray93"><i className="fab fa-github-square icon-size" /></a>
+    <a href="https://twitter.com/wbray93"><i className="fab fa-twitter icon-size" /></a>
   </div>
 );
 
