639239e2e23752239c4b1f0eef3f30d08b696ae8
wbray93 <wbray053@gmail.com>
2019-02-16 00:45:43 -0800

    Added skills section info, Styled all platform based mediaqueries for said skills sections

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,8 +1,36 @@
 import * as React from "react";
 
 const Skills = () => (
-  <div className="skills-section img-responsive">
-  
+  <div>
+  <div className="skills-section">
+    <em><u><h1>My Skills and Experience:</h1></u></em>
+  </div>
+  <div>
+    <ul className="skills-section-ul-padding">
+      <strong><li className="skills-section-list">HTML5: Have a decent mastery of html5 currently experimenting with canvas to make fun simple 
+                  2d games.</li></strong>
+      <strong><li className="skills-section-list">CSS3: One of my favorite things that I have learned. I love styling pages making them super functional and 
+          asthetically pleasing to the eye. I love to make simple and clean layouts or fancy and fine tuned detailed 
+          layouts.</li></strong>
+      <strong><li className="skills-section-list"> Javascript: Something that took me a good 3-4 months to wrap my mind around but now I have a genuine
+                   understanding of the language and am now focused on mastering the final aspects of OOP</li></strong>
+      <strong><li className="skills-section-list">Git: Absolutely love using git by far in my opinion the best version control system around and super 
+                  simple to use.</li></strong>
+      <strong><li className="skills-section-list">Github: My favorite site for repositories and storing my projects as well as working with others such as 
+                  groups from riverside freecodecamp that I meet with once a month to network and work on projects 
+                  together.</li></strong>
+      <strong><li className="skills-section-list">Some React: I am learning to love react through the process of rebuilding this portfolio to just its 
+                  general overall amazing functionality and simple yet clean way of designing ui layouts etc you name it
+                  react can pretty much do it all. Still very much learning this framework but I absolutely 
+                  love it so far.</li></strong>
+      <strong><li className="skills-section-list">jQuery: I can respect jQuery a lot in its simplicity and ability to make writing vanilla js a lot
+                  faster and cleaner while it is no longer the absolute top dog I absolutely do appreciate it and enjoy
+                  working in it when able to.</li></strong>
+      <strong><li className="skills-section-list">Mastering ES6: Currently as with react working on learning the last half of the aspects of ES6.
+                  I enjoy it a lot so far in how it solved some of ES5's problems as well as added a lot of new 
+                  useful features and functionality which makes developing a lot easier.</li></strong>
+    </ul>
+  </div>
   </div>
 );
 
