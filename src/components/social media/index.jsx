66f959ee0261d0e5e1b444ad4357e91a5744b997
wbray93 <wbray053@gmail.com>
2019-02-18 13:50:30 -0800

    updated social media to have updated linked in link

--- src/components/social media/index.jsx
+++ src/components/social media/index.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const Social = () => (
   <div className="social-media">
     <a href="https://www.facebook.com/Whitehawk-Industries-1817186608317870/" target="blank_"><i className="fab fa-facebook icon-size" /></a>
-    <a href="https://www.linkedin.com/in/william-bray-2b47a2157/" target="blank_"><i className="fab fa-linkedin icon-size" /></a>
+    <a href="www.linkedin.com/in/wbray93" target="blank_"><i className="fab fa-linkedin icon-size" /></a>
     <a href="https://github.com/wbray93" target="blank_"><i className="fab fa-github-square icon-size" /></a>
     <a href="https://twitter.com/wbray93" target="blank_"><i className="fab fa-twitter icon-size" /></a>
     <a href="https://whitehawkindustries.blogspot.com/" target="blank_"><i className="fab fa-blogger-b icon-size" /></a>
